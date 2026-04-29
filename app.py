40628d2ebb4340ff5a0f1c50fbb4616c42855dc3
Westes Mesquita da Silva <westesmso@PREFEITURA.SP.GOV.BR>
2026-04-29 13:03:10 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# print('Sabor Express')
+
+# print('1. Cadastrar Restaurante')
+# print('2. Listar Restaurantes')
+# print('3. Ativar Restaurante')
+# print('4. Sair')
+
+# op = input('Escolha uma opção: ')
+
+# print(f'Opção escolhida: {op}')
+
